98568573d8afd86d7cac009a53fc189a02cd25c5
Aron Braggans <aron.braggans@gmail.com>
2021-03-25 21:14:39 -0500

    login

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import {
+  Button,
   Container,
   createStyles,
   Divider,
@@ -11,18 +12,28 @@ import {
   makeStyles,
   MenuItem,
   Select,
-  Table,
+  Slider,
   Typography,
 } from '@material-ui/core';
-import { useEffect, useState } from 'react';
+import { Fragment, useEffect, useState } from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
 
 export default function App() {
   const classes = useStyles();
+  const {
+    loginWithRedirect,
+    isAuthenticated,
+    logout,
+    user,
+    isLoading,
+  } = useAuth0();
   const [list, setList] = useState([]);
   const [sources, setSources] = useState([]);
   const [resorts, setResorts] = useState([]);
   const [filter, setFilter] = useState('');
   const [resort, setResort] = useState('');
+  const [source, setSource] = useState('');
+  const [pointsRange, setPointsRange] = useState([0, 2000]);
 
   const useYear = [
     'January',
@@ -38,6 +49,10 @@ export default function App() {
     'November',
     'December',
   ];
+
+  const handlePointRangeChange = (event, newValue) => {
+    setPointsRange(newValue);
+  };
 
   useEffect(() => {
     fetch('http://localhost:5150/listing', {
@@ -83,9 +98,44 @@ export default function App() {
       });
   }, []);
 
+  const marks = [
+    {
+      value: 0,
+      label: '0',
+    },
+    {
+      value: 50,
+      label: '50',
+    },
+    {
+      value: 75,
+      label: '75',
+    },
+    {
+      value: 150,
+      label: '150',
+    },
+    {
+      value: 200,
+      label: '200',
+    },
+    {
+      value: 500,
+      label: '500',
+    },
+    {
+      value: 1000,
+      label: '1000',
+    },
+    {
+      value: 2000,
+      label: '2000',
+    },
+  ];
+
   return (
     <Container maxWidth="xl">
-      <Grid container>
+      <Grid container spacing={2} alignItems="center">
         <Grid item>
           <FormControl className={classes.formControl} variant="outlined">
             <InputLabel>Resorts</InputLabel>
@@ -98,12 +148,14 @@ export default function App() {
                 <em>None</em>
               </MenuItem>
               {resorts.map((resort) => (
-                <MenuItem key={resort.id} value={resort.name}>
+                <MenuItem key={resort.id} value={resort.id}>
                   {resort.name}
                 </MenuItem>
               ))}
             </Select>
           </FormControl>
+        </Grid>
+        <Grid item>
           <FormControl className={classes.formControl} variant="outlined">
             <InputLabel>Use Year</InputLabel>
             <Select
@@ -122,6 +174,62 @@ export default function App() {
             </Select>
           </FormControl>
         </Grid>
+        <Grid item style={{ flex: 1 }}>
+          <FormControl className={classes.formControl} variant="outlined">
+            <InputLabel>Sites</InputLabel>
+            <Select
+              label="Sites"
+              value={source}
+              onChange={(e) => setSource(e.target.value)}
+            >
+              <MenuItem value="">
+                <em>None</em>
+              </MenuItem>
+              {sources.slice(0, 3).map((source) => (
+                <MenuItem key={source.id} value={source.id}>
+                  {source.name}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </Grid>
+        {isAuthenticated ? (
+          <>
+            <Grid item>
+              <Typography>Welcome Home, {user.name}</Typography>
+            </Grid>
+            <Grid item>
+              <Button
+                variant="contained"
+                onClick={() => logout({ returnTo: window.location.origin })}
+              >
+                Logout
+              </Button>
+            </Grid>
+          </>
+        ) : (
+          <Grid item>
+            <Button variant="contained" onClick={() => loginWithRedirect()}>
+              Log In
+            </Button>
+          </Grid>
+        )}
+      </Grid>
+      <Grid container spacing={2}>
+        <Grid item style={{ width: '100%' }}>
+          <div style={{ width: '100%' }}>
+            <Typography gutterBottom>Points Range</Typography>
+            <Slider
+              value={pointsRange}
+              min={0}
+              max={2000}
+              onChange={handlePointRangeChange}
+              step={25}
+              marks={marks}
+              valueLabelDisplay="on"
+            />
+          </div>
+        </Grid>
       </Grid>
       <Grid container>
         <Grid item>
@@ -131,7 +239,13 @@ export default function App() {
                 if (!resort) {
                   return true;
                 }
-                return r.name === resort;
+                return r.resort.id === resort;
+              })
+              .filter((s) => {
+                if (!source) {
+                  return true;
+                }
+                return s.source.id === source;
               })
               .filter((l) => {
                 if (!filter) {
@@ -139,35 +253,44 @@ export default function App() {
                 }
                 return l.useYear.trim() === filter;
               })
+              .filter((l) => {
+                if (!pointsRange) {
+                  return true;
+                }
+                return l.points >= pointsRange[0] && l.points <= pointsRange[1];
+              })
               .map((listing, i) => {
                 return (
-                  <ListItem key={listing.id}>
-                    <ListItemText
-                      primary={listing.name}
-                      secondary={
-                        <>
-                          <Typography component="span" variant="subtitle1">
-                            {listing.useYear}
-                          </Typography>
-                          {' | '}
-                          <Typography component="span" variant="body1">
-                            {listing.price}
-                          </Typography>
-                          {' | '}
-                          <Typography component="span" variant="body1">
-                            {listing.points}
-                          </Typography>
-                          <Divider />
-                          <Typography component="p" variant="body2">
-                            {listing.description}
-                          </Typography>
-                          <Typography component="p">
-                            {new Date(listing.updatedAt).toDateString()}
-                          </Typography>
-                        </>
-                      }
-                    />
-                  </ListItem>
+                  <Fragment key={listing.id}>
+                    <ListItem dense>
+                      <ListItemText
+                        primary={listing.name}
+                        secondary={
+                          <>
+                            <Typography component="span" variant="subtitle1">
+                              {listing.useYear}
+                            </Typography>
+                            {' | '}
+                            <Typography component="span" variant="body1">
+                              {listing.price}
+                            </Typography>
+                            {' | '}
+                            <Typography component="span" variant="body1">
+                              {listing.points}
+                            </Typography>
+
+                            <Typography component="p" variant="body2">
+                              {listing.description}
+                            </Typography>
+                            <Typography component="p">
+                              {new Date(listing.updatedAt).toDateString()}
+                            </Typography>
+                          </>
+                        }
+                      />
+                    </ListItem>
+                    <Divider variant="inset" component="li" />
+                  </Fragment>
                 );
               })}
           </List>
--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { Auth0Provider } from '@auth0/auth0-react';
+import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      domain="roamdevelopment.auth0.com"
+      clientId="eCq5edMoL2IKUwRi0NyZAnZexfopHlDC"
+      redirectUri={window.location.origin}
+    >
+      <App />
+    </Auth0Provider>
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
